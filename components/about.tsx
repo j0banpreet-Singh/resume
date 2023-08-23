6d35b76108e73a7259cf670a71d8bd51816e0a7f
Joban Arora <jobanarora@Jobans-MacBook-Air.local>
2023-08-22 23:05:53 -0400

    update

--- components/about.tsx
+++ components/about.tsx
@@ -35,8 +35,8 @@ export default function About() {
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">Sikhism Religion</span>. I'm also
+        a gymLover.
       </p>
     </motion.section>
   );
